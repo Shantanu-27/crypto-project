3695c55725a56b3c5a60b4245c285e78434b372f
shantanuchaudhary <shantanuchaudhary317@gmail.comgit config --global user.email shantanuchaudhary317@gmail.comgit config --global user.email shantanuchaudhary317@gmail.com>
2024-09-05 05:00:48 +0530

    ..

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import {Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Dashboard from './pages/Dashboard';
 import Coin from './pages/Coin';
